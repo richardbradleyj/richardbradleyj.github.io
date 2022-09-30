0b6b62019907ad1e161d5861b9f4eaf8608694f8
Richard Bradley <richardbradleyj@gmail.com>
2022-09-29 18:03:21 -0700

    update

--- JS/Sample_Portfolio.js
+++ JS/Sample_Portfolio.js
@@ -21,12 +21,12 @@ function showSlides (n) {
     var slides = document.getElementsByClassName ("mySlides"); 
     var dots = document.getElementsByClassName ("dot"); 
     if (n > slides.length) {slideIndex = 1};
-    if (n > 1) {slideIndex = slides.length};
-    for (i = 0; i <slides.length; i++) {
-        slides[i].style.disply ="none";
+    if (n < 1) {slideIndex = slides.length};
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.disply = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active");
+        dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
